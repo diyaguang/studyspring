28847eabe3a58592a7613ef28787d2262674ec6e
diyaguang <diyaguang@163.com>
2019-04-18 10:31:58 +0800

    add new code

---
+++ src/com/dygstudio/studyspring/assemble/User.java
@@ -0,0 +1,53 @@
+package com.dygstudio.studyspring.assemble;
+
+import java.util.List;
+
+/**
+ * @Project: studyspring
+ * @Author: diyaguang
+ * @CreateDate: 2019-04-18-10:24
+ * @Description:
+ */
+public class User {
+    private String username;
+    private Integer password;
+    private List<String> list;
+    public User(String username,Integer password,List<String> list){
+        super();
+        this.username = username;
+        this.password = password;
+        this.list = list;
+    }
+    public User(){
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getPassword() {
+        return password;
+    }
+
+    public void setPassword(Integer password) {
+        this.password = password;
+    }
+
+    public List<String> getList() {
+        return list;
+    }
+
+    public void setList(List<String> list) {
+        this.list = list;
+    }
+
+    @Override
+    public String toString(){
+        return "User [username="+username+", password="+password+", list="+list+"]";
+    }
+}
